0ae107355007c6ab13b17f6812cd201d4c7fdf71
Vivek Mecharla <vivekmecharla@github.com>
2018-05-31 21:06:06 +0200

    Initial Commit

---
+++ src/main/java/com/vivek/giflibrary/GiflibraryApplication.java
@@ -0,0 +1,12 @@
+package com.vivek.giflibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiflibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiflibraryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vivek/giflibrary/GiflibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vivek.giflibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GiflibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
